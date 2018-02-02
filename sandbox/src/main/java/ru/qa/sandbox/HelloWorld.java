6c2eb0d4ec49a860359877cdb58ad4bf67959d45
Vasilijasin <vasilijasin1965@gmail.com>
2018-02-02 11:52:58 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/qa/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/qa/sandbox/HelloWorld.java
@@ -1,7 +1,32 @@
 package ru.qa.sandbox;
 
+import static ru.qa.sandbox.HelloWorld.hello;
+
 class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       // String somebody =  "world";
+        hello("world");
+        hello("user");
+        hello("Maks");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+/*        double l = 8;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);*/
+    }
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area (double len){
+        return len* len;
+    }
+    public static double area(double a, double b){
+        return a * b;
     }
 }   
